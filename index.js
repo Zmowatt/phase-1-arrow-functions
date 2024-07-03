339cd9015b38a7187671a6bf04d2da9fe1dcd504
Zachary Mowatt <zamowatt@gmail.com>
2024-07-02 20:13:48 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const divide = function(x, y) {
-    return x/y
-}
+const divide = function(num1, num2){
+    return num1/num2
+};
 
-divide(2000, 100)
+divide(2000,100);
 
-square = param1 => param1 * param1;
+const square = (num3) => num3*num3;
 
-add = (param2, param3) => param2 + param3;
+const add = (num4, num5) => num4 + num5;
